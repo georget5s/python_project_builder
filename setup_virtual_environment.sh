204f3ac4dfdf733fe7b5182c474937631ac76863
George Tsiones <gtsiones@localhost.localdomain>
2021-04-01 20:45:33 -0400

    Added the beginning of the setup script

---
+++ setup_virtual_environment.sh
@@ -0,0 +1,27 @@
+#/bin/bash
+
+source_repo=$1
+branch=$2
+dist_repo=$3
+application=$4
+build_id=`date +%s`
+build_dir=/tmp/build_${application}_${build_id}
+project_source=${build_dir}/source
+venv_dir=${build_dir}/env
+
+if [[ "$branch" != "" ]]; then
+	branch_clause="-b $branch"
+fi
+
+mkdir -p $project_source
+mkdir -p $venv_source
+
+# Download Source
+
+git clone $branch_clause $source_repo $project_source 
+
+#Create a python virtual environment
+virtualenv $venv_dir
+source $venv_dir/bin/activate
+
+deactivate
