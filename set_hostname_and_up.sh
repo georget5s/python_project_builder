616daedec25066b0bb2eb99135effdf7e98cd343
George Tsiones <63453526+georget5s@users.noreply.github.com>
2021-04-07 19:26:46 -0500

    Create set_hostname_and_up.sh

---
+++ set_hostname_and_up.sh
@@ -0,0 +1,40 @@
+#!/bin/bash
+
+hostname=$1
+ip=$2
+
+echo "Setting hostname to [$hostname] and ip address to [$ip], continue [y/n]"
+read continue
+
+if [[ $continue != "y" ]]; then
+        echo "Exiting, no action taken..."
+fi
+
+echo "$1" > /etc/hostname
+
+rip='10\.0\.2\.200'
+nip=`echo "$ip" | sed 's/\\./\\\./g'`
+cat << EOF | sed "s/$rip/$nip/g" > /etc/sysconfig/network-scripts/ifcfg-enp0s3
+TYPE="Ethernet"
+PROXY_METHOD="none"
+BROWSER_ONLY="no"
+BOOTPROTO="dhcp"
+DEFROUTE="yes"
+IPV4_FAILURE_FATAL="no"
+IPV6INIT="yes"
+IPV6_AUTOCONF="yes"
+IPV6_DEFROUTE="yes"
+IPV6_FAILURE_FATAL="no"
+IPV6_ADDR_GEN_MODE="stable-privacy"
+NAME="enp0s3"
+UUID="e69c471d-f596-4690-811c-a11737125b70"
+DEVICE="enp0s3"
+ONBOOT="yes"
+IPV6_PRIVACY="no"
+PREFIX="24"
+IPADDR="10.0.2.200"
+GATEWAY="10.0.2.2"
+EOF
+
+nmcli network off
+nmcli network on
