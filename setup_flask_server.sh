7ddb379878ea9fdbfcfa50a2de976380af732739
George Tsiones <63453526+georget5s@users.noreply.github.com>
2021-04-07 20:29:04 -0500

    Create setup_flask_server.sh

---
+++ setup_flask_server.sh
@@ -0,0 +1,48 @@
+#!/bin/bash
+echo "Install httpd..."
+dnf install httpd
+echo "Install httpd-devel..."
+dnf install httpd-devel
+echo "Install python3..."
+dnf install python3
+echo "Install python3-pip..."
+dnf install python3-pip
+echo "Install python3-mod-wsgi..."
+dnf install python3-mod_wsgi
+
+
+echo "Open port 80..."
+firewall-cmd --permanent --add-port=80/tcp
+firewall-cmd --reload
+
+echo "Write out wsgi configuration..."
+cat > /etc/httpd/conf.d/python-wsgi.conf << __WSGICONF__
+WSGIDaemonProcess myapp processes=1 threads=10 display-name=%{GROUP} python-home=/opt/gtsi/sample_flask/venv
+
+WSGIProcessGroup myapp
+WSGIApplicationGroup %{GLOBAL}
+
+WSGIScriptAlias / /var/www/html/app.py
+
+<Directory /var/www/html>
+    Order allow,deny
+    Allow from all
+</Directory>
+__WSGICONF__
+
+echo "Write bootstrap flask app..."
+cat > /var/www/html/app.py << __FLASK_BOOT_STRAP__
+from gtsiweb import flask_app
+if __name__ == "__main__":
+    flask_app.app.run()
+__FLASK_BOOT_STRAP__
+
+echo "Enable httpd..."
+systemctl enable httpd
+
+# Needed to allow wsgi daemon to do outbound connections
+echo "Set wsgi to allow outbound connections..."
+/usr/sbin/setsebool -P httpd_can_network_connect 1
+
+echo "Install java..."
+dnf install java
